5e8eca30a491ba8985d64526bc934021ac9b832d
Famon1 <37359739+Famon1@users.noreply.github.com>
2020-02-29 18:46:39 +0300

    create

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Обучение. Пакет 1 - Базовый</title>
+</head>
+<body>
+<div id='main'></div>
+  <!--<input type="range" min="1" max="5" step="1" value="5" oninput="ballch(this.value)" width='100px'>-->
+</body>
+<?include('scriptlib.html')?>
+</html>
