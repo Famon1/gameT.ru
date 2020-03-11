9d03383bb92a85b5427d0e4445dede585689fda3
Famon1 <37359739+Famon1@users.noreply.github.com>
2020-03-11 20:04:49 +0300

    add gimp and libre

--- index.php
+++ index.php
@@ -5,11 +5,38 @@
   <title>Обучение. Пакет 1 - Базовый</title>
 </head>
 <body>
-<div id='main_index' class="container-fluid" style='font-size:38px;'>
- <button type="button" class="btn-primary w-100 p-2 mt-5" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(1)">Тренажер 1. Скоростной клик мыши</button><br>
- <button type="button" class="btn-primary w-100 p-2 mt-5" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(2)">Тренажер 2. Перетаскивание элементов</button><br>
- <button type="button" class="btn-primary w-100 p-2 mt-5" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(3)">Тренажер 3. Работа с клавиатурой</button>
+<div id='main_index' class="container-fluid" style='font-size:24px;'>
+ <button type="button" class="btn-primary w-100 p-1 mt-3" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(1)">Тренажер 1. Скоростной клик мыши</button><br>
+ <button type="button" class="btn-primary w-100 p-1 mt-3" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(2)">Тренажер 2. Перетаскивание элементов</button><br>
+ <button type="button" class="btn-primary w-100 p-1 mt-3" data-toggle="modal" data-target="#exampleModal" onclick="set_modal(3)">Тренажер 3. Работа с клавиатурой</button>
+
+ <hr />
+<div class="row">
+<div class="col-sm-6">
+ <div class="card">
+  <img src="img/gimp.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text" style='font-size:18px;'>Изучение графического редактора GIMP. Рисование и фотообработка.</p>
+    <a href="http://www.progimp.ru/" class="btn btn-primary">Сайт с учебным материалом</a>
+    <a href="https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.18-setup.exe" class="btn btn-primary">Скачать программу</a>
+  </div>
+  </div>
+ </div>
+
+<div class="col-sm-6">
+  <div class="card">
+   <img src="img/loff.jpg" class="card-img-top">
+   <div class="card-body">
+     <p class="card-text" style='font-size:18px;'>Бесплатный офисный пакет LibreOffice. Создание и редактирование документов, таблиц, презентаций и баз данных.</p>
+     <a href="https://mostik.info/besplatniy_kurs_libreoffice/" class="btn btn-primary">Сайт с учебным материалом</a>
+     <a href="http://download.documentfoundation.org/libreoffice/stable/6.4.1/win/x86/LibreOffice_6.4.1_Win_x86.msi" class="btn btn-primary">Скачать программу</a>
+   </div>
+ </div>
+ </div>
 </div>
+</div>
+
+
 <!-- Modal -->
 <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
